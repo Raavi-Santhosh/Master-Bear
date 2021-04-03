160c85758533a05f55a05291115f1dd4105f041a
SantuSanthosh <raavisanthosh@gmail.com>
2021-04-03 15:02:12 +0530

    Added all the available md docs to matlab fundass

--- sidebars.js
+++ sidebars.js
@@ -1,28 +1,37 @@
+
 module.exports = {
 
   "MATLAB Fundass!": {
     "Matlab Environment": [
         "matlab/1-Matlab-Environment",
         "matlab/2-Matlab-Basic-Syntax",
+        "matlab/3-Data-Types",
+        "matlab/4-Input-&-Output-Operations",
+        "matlab/5-Operators",
+        "matlab/6-Conditional-Statements",
+        "matlab/7-Colon-Operator",
+        "matlab/8-Loops",
+        "matlab/9-Functions",
+        // "matlab/",
       ],
     },
   
 
-  docs: [
-    {
+  // docs: [
+  //   {
       
-      type: 'category',
-      label: 'Docs!',
-      items: [
-        'getting-started',
-        'create-a-page',
-        'create-a-document',
-        'create-a-blog-post',
-        'markdown-features',
-        'thank-you',
-      ],
-    },
-  ],
+  //     type: 'category',
+  //     label: 'Docs!',
+  //     items: [
+  //       'getting-started',
+  //       'create-a-page',
+  //       'create-a-document',
+  //       'create-a-blog-post',
+  //       'markdown-features',
+  //       'thank-you',
+  //     ],
+  //   },
+  // ],
 };
 
 
